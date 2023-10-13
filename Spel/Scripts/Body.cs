90fcbe406713b1a445d8c31ea9fbd729781c1b1b
Kalle Wiklund <kallewiklund2002@gmail.com>
2023-10-13 23:01:36 +0200

    Start of a general body to be inhabitated

---
+++ Spel/Scripts/Body.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class Body : CharacterBody2D
+{
+	private float _runSpeed = 350;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    public override void _PhysicsProcess(double delta) {
+		GetInput();
+		MoveAndSlide();
+    }
+
+	private void GetInput() {
+		var velocity = new Vector2(0, 0);
+
+		var right = Input.IsActionPressed("ui_right");
+		var left = Input.IsActionPressed("ui_left");
+		var up = Input.IsActionPressed("ui_up");
+		var down = Input.IsActionPressed("ui_down");
+
+		if (right) velocity.X += _runSpeed;
+		if (left) velocity.X -= _runSpeed;
+		if (up) velocity.Y -= _runSpeed;
+		if (down) velocity.Y += _runSpeed;
+
+		Velocity = velocity;
+	}
+}
