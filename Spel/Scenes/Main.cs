faf9b5d5eb4e0e43c7f7a23774e16e254c62e385
Krysset <maxhagman@hotmail.se>
2023-10-13 22:23:40 +0200

    Added player and main scene

---
+++ Spel/Scenes/Main.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		NewGame();
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	public void NewGame()
+	{
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+	}
+}
