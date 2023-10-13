d7a1cda39f1eda5fca8e1f2276f8509c8a1e8b43
Kalle Wiklund <kallewiklund2002@gmail.com>
2023-10-14 01:16:45 +0200

    idk small stuff lol

--- Spel/Scripts/Body.cs
+++ Spel/Scripts/Body.cs
@@ -3,16 +3,17 @@ using System;
 
 public partial class Body : CharacterBody2D
 {
+	private bool isPossessed;
 	private float _runSpeed = 350;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-
+		this.isPossessed = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+	public override void _Process(double delta) {
+		
 	}
 
     public override void _PhysicsProcess(double delta) {
