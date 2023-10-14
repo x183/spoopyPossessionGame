ff4b8438355a6bfa8f9bf20dbadf0d83e2e38b4e
Kalle Wiklund <kallewiklund2002@gmail.com>
2023-10-14 02:19:59 +0200

    first draft of PlayerController

--- Spel/Scripts/Body.cs
+++ Spel/Scripts/Body.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Body : CharacterBody2D
 {
 	private bool isPossessed;
-	private float _runSpeed = 350;
+	public float _runSpeed { get; set; } = 350;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
@@ -13,27 +13,19 @@ public partial class Body : CharacterBody2D
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
-		
+		if (this.isPossessed) {
+			GetInput();
+		}
 	}
 
     public override void _PhysicsProcess(double delta) {
-		GetInput();
-		MoveAndSlide();
+		if (this.isPossessed) {
+			MoveAndSlide();
+		}
     }
 
 	private void GetInput() {
-		var velocity = new Vector2(0, 0);
+		var velocity = Vector2.Zero;
 
-		var right = Input.IsActionPressed("ui_right");
-		var left = Input.IsActionPressed("ui_left");
-		var up = Input.IsActionPressed("ui_up");
-		var down = Input.IsActionPressed("ui_down");
-
-		if (right) velocity.X += _runSpeed;
-		if (left) velocity.X -= _runSpeed;
-		if (up) velocity.Y -= _runSpeed;
-		if (down) velocity.Y += _runSpeed;
-
-		Velocity = velocity;
 	}
 }
