f888fa1cb6c4e88ac1f064aa2f81f3080b871933
Oscar Palm <palmoscar4@gmail.com>
2023-10-14 04:01:16 +0200

    Quits on touching of bodies

--- Spel/Scenes/Creatures/Ghost/GhostBuster.cs
+++ Spel/Scenes/Creatures/Ghost/GhostBuster.cs
@@ -3,43 +3,39 @@ using System;
 
 public partial class GhostBuster : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
-
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+	[Export]
+	public CharacterBody2D player;
+	public const float Speed = 100.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
+		Vector2 position = Position;
+		Vector2 playerPosition = player.Position;
+		Vector2 direction = (playerPosition - position);
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+		Vector2 normDir = direction.Normalized();
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-			velocity.Y = JumpVelocity;
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
-
+		velocity = normDir * Speed;
 		Velocity = velocity;
 		MoveAndSlide();
+		for (int i = 0; i < GetSlideCollisionCount(); i++)
+		{
+			KinematicCollision2D collision = GetSlideCollision(i);
+			if ((collision.GetCollider() as Node).Name == "Player") GetTree().Quit();
+		}
 	}
 
 	public void Start(Vector2 position)
 	{
 		Position = position;
 		Show();
+	}
+
+	private void OnBodyEntered(PhysicsBody2D body)
+	{
+		GD.Print("Body entered");
+		if (body == player)
+			GetTree().Quit();
 	}
 }
